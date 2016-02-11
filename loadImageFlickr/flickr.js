0089b51d5a3b45ee809564691e20f59aa2271ac1
jmsaavedra <saavedra@jos.ph>
2016-02-11 12:16:41 +0100

    added loadImageFlickr

--- loadImageFlickr/flickr.js
+++ loadImageFlickr/flickr.js
@@ -4,26 +4,27 @@ var API_KEY = '2d624fff40205edcdcf5155d276e9c89';
 var PIETER_USER_ID = '139152471@N06';
 var JOE_USER_ID = '43854009@N05';
 
-var photoQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+API_KEY+"&jsoncallback=?";
-var exifQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key="+API_KEY+"&jsoncallback=?";
+// var photoQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+API_KEY+"&jsoncallback=?";
+var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+var geoQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key="+API_KEY+"&jsoncallback=?";
+//var exifQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key="+API_KEY+"&jsoncallback=?";
 
 //size of photos to query for: https://www.flickr.com/services/api/misc.urls.html
 var PHOTO_SIZE = 'm'; //c = medium (800px longest side), o = original
 
 function queryFlickr(callback){
-  // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  // var flickrQuery = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+API_KEY+"&tags=flower&per_page=3&format=json";
-
-
-  $.getJSON( photoQuery, {
-    // tags: "gluon,winterlab",
-    // tagmode: "all",
-    user_id: PIETER_USER_ID,
-    per_page: 3,
+  $.getJSON( flickerAPI, {
+    // user_id: PIETER_USER_ID,
+    // tags: "gluon,winterlab,2016",
+    tags: 'iphoneography,Lark,abstract,diagonal,geometric,minimalism,foliage',
+    tag_mode: "all", //any
+    per_page: 5,
+    extras: 'date_taken',
     format: "json"
   })
   .done(function(data){
-    queryExifs(data.photos.photo, callback);
+    console.log('data:'+JSON.stringify(data));
+    // queryExifs(data.photos.photo, callback);
   });
 }
 
@@ -40,33 +41,44 @@ function queryExifs(photos, cb){
 
   function getExif(photo, cb){
     // console.log('querying EXIF for: '+JSON.stringify(photo, null, '\t'));
+    console.log('getting GEO for id: '+photo.id);
     var photoId = photo.id;
     var photoData = {
       id: photoId,
       url: 'https://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_'+PHOTO_SIZE+'.jpg'
     };
 
-    $.getJSON( exifQuery, {
+    $.getJSON( geoQuery, {
       photo_id: photoId,
+      extras: "url_c", /* NOT WORKING WTF */
       format: "json"
     })
     .done(function(thisPhoto){
-      // console.log(thisPhoto.photo.exif);
-      async.eachSeries(thisPhoto.photo.exif, function(data, callb){
-        if(data.tag === 'GPSLatitude'){
-          // console.log('GOT LATITUDE:', data.clean._content);
-          photoData.lat = data.clean._content;
-        } else if(data.tag === 'GPSLongitude'){
-          // console.log('GOT LONGITUDE:', data.clean._content);
-          photoData.lon = data.clean._content;
-        } else if(data.tag === 'CreateDate'){
-          // console.log('CREATEDATE:', data.raw._content);
-          photoData.date = data.raw._content;
-        }
-        callb();
-      }, function(e){
-        cb(null, photoData);
-      });
+      console.log(thisPhoto);
+      if(thisPhoto.stat === 'ok'){
+        photoData.lat = thisPhoto.photo.location.latitude;
+        photoData.lon = thisPhoto.photo.location.longitude;
+      }
+      cb(null, photoData);
     });
   }
 }
+
+
+
+      // async.eachSeries(thisPhoto.photo.exif, function(data, callb){
+      //   if(data.tag === 'GPSLatitude'){
+      //     // console.log('GOT LATITUDE:', data.clean._content);
+      //     photoData.lat = data.clean._content;
+      //   } else if(data.tag === 'GPSLongitude'){
+      //     // console.log('GOT LONGITUDE:', data.clean._content);
+      //     photoData.lon = data.clean._content;
+      //   } else if(data.tag === 'CreateDate'){
+      //     // console.log('CREATEDATE:', data.raw._content);
+      //     photoData.date = data.raw._content;
+      //   }
+      //   callb();
+      // }, function(e){
+      //   console.log('-- success photoId: '+photoId);
+      //   cb(null, photoData);
+      // });
--- loadImageFlickr/sketch.js
+++ loadImageFlickr/sketch.js
@@ -7,16 +7,14 @@ var loaded = false;
 function preload(){ // doordat javascript asynchroon is moet je zeker zijn dat je beelden eerst geladen worden.
 
   queryFlickr(function(imgs){
-    console.log('>>> got photos ! :', JSON.stringify(imgs, null, '\t'));
+    console.log('>>> got photos :', JSON.stringify(imgs, null, '\t'));
 
     imgs.forEach(function(img, i){
-    /*img: {
-      "id": "24764365331",
+      /*img: { "id": "24764365331",
 		  "url": "https://farm2.staticflickr.com/1481/24764365331_351e00dfdb_m.jpg",
 		  "date": "2016:02:06 14:43:05",
 		  "lat": "50 deg 51' 0.34\" N",
-		  "lon": "4 deg 20' 59.51\" E"
-    }*/
+		  "lon": "4 deg 20' 59.51\" E" }*/
       imagesData.push(img);
       images.push(loadImage(img.url));
     });
@@ -27,6 +25,9 @@ function preload(){ // doordat javascript asynchroon is moet je zeker zijn dat j
 
 function setup() {
   createCanvas(1000,800);
+  background(0);
+  fill(255);
+  text("Loading images from Flickr...", 100, windowHeight/2);
 }
 
 function draw() {
@@ -34,20 +35,23 @@ function draw() {
   
   //only after loaded has completed, draw stuff
   if(loaded){
+    var currX = 0;
     images.forEach(function(img, i){
+      fill(255);
       
       // draw this img
-      fill(255);
-      image(img, img.width*i, 0, img.width, img.height);
+      image(img, currX, 0, img.width, img.height);
       
       // draw img data
       fill(0);
-      text("id: "+imagesData[i].id, img.width*i, img.height+15 );
-      text("date: "+imagesData[i].date, img.width*i+5, img.height+30 );
+      text("id: "+imagesData[i].id, currX+5, img.height+15 );
+      text("date: "+imagesData[i].date, currX+5, img.height+30 );
       if(imagesData[i].lat){ //did we find lat/lon ?
-        text("lat: "+imagesData[i].lat, img.width*i+5, img.height+45 );
-        text("lon: "+imagesData[i].lon, img.width*i+5, img.height+60 );
+        text("lat: "+imagesData[i].lat, currX+5, img.height+45 );
+        text("lon: "+imagesData[i].lon, currX+5, img.height+60 );
       }
+      
+      currX += img.width; //add this img width to the X
     });
   }
 }
